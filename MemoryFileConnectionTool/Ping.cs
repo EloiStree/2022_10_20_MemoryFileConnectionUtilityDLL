2e6d80249a979dd8b19ab3dd489ba73691d84d5f
Éloi Strée <106495897+EloiStree3D@users.noreply.github.com>
2022-10-20 23:20:51 +0200

    save first draft

---
+++ MemoryFileConnectionTool/Ping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryFileConnectionUtility
+{
+    public class HelloPing
+    {
+        public static MemoryFileConnection c;
+        public string PingMessage(string variable) { return "Ping " + variable; }
+        public int PingMessage(int variable) { return 1 + variable; }
+        public string ReadMemory(string memoryName) {
+
+            if (c == null) { 
+                c = new MemoryFileConnection();
+                c.SetNameAndSizeThenReset(memoryName, 100000);
+            }
+            c.GetAsText(out string t);
+            return t;
+        
+        }
+    }
+}
---
+++ MemoryFileConnectionUtilityDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using MemoryFileConnectionUtility;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace MemoryFileConnectionUtilityDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string toPush="Hello World !";
+           
+            MemoryFileConnection connectionPush = new MemoryFileConnection();
+            connectionPush.SetNameAndSizeThenReset("Test1", 100000);
+
+            MemoryFileConnection connectionRecovert = new MemoryFileConnection();
+            connectionRecovert.SetNameAndSizeThenReset("Test1", 100000);
+
+
+            Console.WriteLine("Push: "+ toPush);
+            connectionPush.SetText(toPush);
+
+            connectionRecovert.GetAsText(out string t);
+            Console.WriteLine("Recovert: " + t);
+
+            while (true) {
+                toPush = Console.ReadLine();
+
+                Console.WriteLine("Push: " + toPush);
+                connectionPush.SetText(toPush);
+            }
+        }
+    }
+}
